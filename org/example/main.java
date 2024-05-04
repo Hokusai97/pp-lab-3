19cd5e999bfad4bb7201c4dd935b570fb33deefa
Hokusai97 <rw.rafalwiacek@gmail.com>
2024-05-04 12:58:42 +0200

    Zadanie 1

---
+++ org/example/main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.awt.*;
+import java.io.Console;
+
+public class main {
+    public static void main(String[] args) {
+        Point point = new Point(10,10);
+        System.out.printf("Point coords: x:%s : y:%s%n",point.getX(), point.getY());
+    }
+}
